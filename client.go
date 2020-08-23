5dedef664d41d42720008ab4f06ed6b184e61b0e
Сергей Квартников <s.kvartnikov@butik.ru>
2020-08-23 17:12:58 +0300

    Added getcontacts api method

--- client.go
+++ client.go
@@ -6,6 +6,7 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
@@ -24,7 +25,9 @@ const maxLengthWSSE = 32
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-const host = "https://api.emarsys.net/api/"
+const defaultHost = "https://api.emarsys.net/api/"
+
+const mysqlDateFormat = "2006-01-02"
 
 const (
 	requestPost = iota + 1
@@ -32,10 +35,29 @@ const (
 	requestPut
 )
 
+type ClientOptions func(o *options)
+
 type RequestMethod int
 
 type ClientError struct {
 	err error
+}
+
+type options struct {
+	client *http.Client
+	host   string
+}
+
+func WithCustomHost(host string) ClientOptions {
+	return func(options *options) {
+		options.host = strings.TrimRight(host, "/") + "/"
+	}
+}
+
+func WithClientTimeout(t time.Duration) ClientOptions {
+	return func(options *options) {
+		options.client.Timeout = t
+	}
 }
 
 func (clientError *ClientError) Error() string {
@@ -47,8 +69,9 @@ func newClientError(err error) error {
 }
 
 type Client struct {
-	auth auth
-	host string
+	auth   auth
+	client *http.Client
+	host   string
 }
 
 type Request struct {
@@ -59,19 +82,30 @@ type Request struct {
 
 type ClientInterface interface {
 	Send(r *Request) ([]byte, error)
+	SendIO(r *Request) (io.ReadCloser, error)
 }
 
-func NewClient(user string, secret string) ClientInterface {
+func NewClient(user string, secret string, clientOptions ...ClientOptions) ClientInterface {
+	defaultOptions := &options{
+		client: http.DefaultClient,
+		host:   strings.TrimRight(defaultHost, "/") + "/",
+	}
+
+	for _, f := range clientOptions {
+		f(defaultOptions)
+	}
+
 	return &Client{
 		auth: auth{
 			User:   user,
 			Secret: secret,
 		},
-		host: host,
+		client: defaultOptions.client,
+		host:   defaultOptions.host,
 	}
 }
 
-func (c *Client) Send(r *Request) ([]byte, error) {
+func (c *Client) SendIO(r *Request) (io.ReadCloser, error) {
 	method := ""
 
 	switch r.Method {
@@ -85,7 +119,11 @@ func (c *Client) Send(r *Request) ([]byte, error) {
 		return nil, fmt.Errorf("unknown method: %d", r.Method)
 	}
 
-	serverUrl, _ := url.Parse(c.host + strings.Trim(r.Path, "/"))
+	serverUrl, err := url.Parse(c.host + strings.Trim(r.Path, "/"))
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse url: %s", err)
+	}
+
 	serverUrl.Path += "/"
 
 	req, err := http.NewRequest(method, serverUrl.String(), bytes.NewBuffer(r.Body))
@@ -96,20 +134,27 @@ func (c *Client) Send(r *Request) ([]byte, error) {
 	req.Header.Set("X-WSSE", c.getWSSEHeader())
 	req.Header.Set("Content-Type", "application/json")
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	resp, err := c.client.Do(req)
 	if err != nil {
 		return nil, newClientError(err)
 	}
 
-	defer resp.Body.Close()
-
-	responseBody, _ := ioutil.ReadAll(resp.Body)
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("error response: %s", responseBody)
+		return nil, fmt.Errorf("error response, code: %d", resp.StatusCode)
 	}
 
-	return responseBody, nil
+	return resp.Body, nil
+}
+
+func (c *Client) Send(r *Request) ([]byte, error) {
+	stream, err := c.SendIO(r)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() { _ = stream.Close() }()
+
+	return ioutil.ReadAll(stream)
 }
 
 func (c *Client) getWSSEHeader() string {
--- users_test.go
+++ users_test.go
@@ -3,12 +3,13 @@ package gomarsys
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/require"
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 )
 
 func TestUsers_Create(t *testing.T) {
@@ -108,22 +109,96 @@ func TestUsers_GetChanges(t *testing.T) {
 	}).Return([]byte(`{"replyCode": 0,"replyText" :"ok","data":{"id": 123}}`), nil)
 
 	request := ChangesRequest{
-		DistributionMethod: DistributionMethodLocal,
-		Origin:             OriginAll,
-		OriginID:           "0",
+		BaseExportRequest: BaseExportRequest{
+			DistributionMethod: DistributionMethodLocal,
+			ContactFields: []int{
+				EMail,
+				OptIn,
+			},
+			Delimiter: ",",
+		},
+		Origin:   OriginAll,
+		OriginID: "0",
 		TimeRange: []string{
 			time.Now().Format("2006-01-02 15:04"),
 			time.Now().Format("2006-01-02 15:04"),
 		},
-		ContactFields: []int{
-			EMail,
-			OptIn,
-		},
-		Delimiter: ",",
 	}
 
 	user := NewUsers(client)
 	changes, err := user.GetChanges(request)
+	require.NoError(t, err)
+	assert.Equal(t, changes.ReplyCode, 0)
+	assert.Equal(t, changes.ReplyText, "ok")
+	assert.Equal(t, changes.Data.ID, 123)
+}
+
+func TestUsers_GetContacts(t *testing.T) {
+	client := NewClientMock()
+	client.(*ClientMock).On("Send", mock.Anything).Run(func(args mock.Arguments) {
+		req := args.Get(0).(*Request)
+
+		assert.Equal(t, req.Path, "/v2/email/getcontacts")
+		assert.Equal(t, req.Method, RequestMethod(requestPost))
+		var v ContactRequest
+		err := json.NewDecoder(strings.NewReader(string(req.Body))).Decode(&v)
+		require.NoError(t, err)
+
+		assert.Equal(t, v.DistributionMethod, DistributionMethodLocal)
+		assert.Equal(t, v.ContactListID, 123)
+		assert.Equal(t, v.ContactFields, []int{EMail, OptIn})
+	}).Return([]byte(`{"replyCode": 0,"replyText" :"ok","data":{"id": 123}}`), nil)
+
+	request := ContactRequest{
+		BaseExportRequest: BaseExportRequest{
+			DistributionMethod: DistributionMethodLocal,
+			ContactFields: []int{
+				EMail,
+				OptIn,
+			},
+			Delimiter: ",",
+		},
+		ContactListID: 123,
+	}
+
+	user := NewUsers(client)
+	changes, err := user.GetContacts(request)
+	require.NoError(t, err)
+	assert.Equal(t, changes.ReplyCode, 0)
+	assert.Equal(t, changes.ReplyText, "ok")
+	assert.Equal(t, changes.Data.ID, 123)
+}
+
+func TestUsers_GetSegments(t *testing.T) {
+	client := NewClientMock()
+	client.(*ClientMock).On("Send", mock.Anything).Run(func(args mock.Arguments) {
+		req := args.Get(0).(*Request)
+
+		assert.Equal(t, req.Path, "/v2/export/filter")
+		assert.Equal(t, req.Method, RequestMethod(requestPost))
+		var v SegmentRequest
+		err := json.NewDecoder(strings.NewReader(string(req.Body))).Decode(&v)
+		require.NoError(t, err)
+
+		assert.Equal(t, v.DistributionMethod, DistributionMethodLocal)
+		assert.Equal(t, v.Filter, 123)
+		assert.Equal(t, v.ContactFields, []int{EMail, OptIn})
+	}).Return([]byte(`{"replyCode": 0,"replyText" :"ok","data":{"id": 123}}`), nil)
+
+	request := SegmentRequest{
+		BaseExportRequest: BaseExportRequest{
+			DistributionMethod: DistributionMethodLocal,
+			ContactFields: []int{
+				EMail,
+				OptIn,
+			},
+			Delimiter: ",",
+		},
+		Filter: 123,
+	}
+
+	user := NewUsers(client)
+	changes, err := user.GetSegment(request)
 	require.NoError(t, err)
 	assert.Equal(t, changes.ReplyCode, 0)
 	assert.Equal(t, changes.ReplyText, "ok")
